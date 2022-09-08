d142be82da59f09a6b0fd593a900728dd6412d64
MariaCosmos <cosmos.maria2003@gmail.com>
2022-09-08 12:55:43 -0300

    Exercicios concluídos - condicionais II

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 //código a ser reescrito
 
-if(nome === "José"){
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
@@ -10,4 +10,15 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+//mesmo código utilizando if ternário
+
+const nome = prompt("insira seu nome")
+
+nome === "José" ? console.log("Oi, Zé!") : console.log(`Olá, ${nome}`)
+
+
+const idade = prompt("insira sua idade")
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista")
